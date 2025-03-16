1092e2edf84842cc8c54c4fe0af951845406c3c7
aldair-meneses <aldair.meneses@protonmail.com>
2025-03-16 17:22:11 -0300

    adiciona configuração de base para o Vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
+  base: '/barbearia98/site/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
