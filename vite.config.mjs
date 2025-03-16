c9d0e761fd68919b93d8122da506db6b707d1db9
aldair-meneses <aldair.meneses@protonmail.com>
2025-03-16 19:49:47 -0300

    remove vite-plugin-image-optimizer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   build: {
@@ -9,17 +8,5 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
-    ViteImageOptimizer({
-      png: { quality: 80 },
-      jpeg: { quality: 75 },
-      webp: { quality: 80 },
-      avif: { quality: 70 },
-      svg: {
-          plugins: [
-              { name: 'removeViewBox', active: false },
-              { name: 'sortAttrs' },
-          ],
-      },
-  }),
   ],
 })
